75cf2cd2ca89547780a42165936cc1f90cc00f02
Emiliia <emiliatoropova@yandex.ru>
2023-02-10 21:52:36 +0200

    Добавляет компоненты для навбара

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <navigation-logo class="logo"></navigation-logo>
+<!--    TODO: переименовать в "открыть/закрыть меню" при автоматизации-->
+    <button class="button button--navigation">Переключить меню</button>
+    <navigation-list class="navigation"></navigation-list>
+  </header>
+</template>
+
+<script>
+import NavigationLogo from "@/components/Navbar/components/NavigationLogo.vue";
+import NavigationList from "@/components/Navbar/components/NavigationList.vue";
+
+export default {
+  name: "Navbar",
+  components: {NavigationList, NavigationLogo}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

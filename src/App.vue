22079a7abe40aeddc49072907127e14ea51c38d2
Emiliia <emiliatoropova@yandex.ru>
2023-02-10 20:27:08 +0200

    Разворачивает проект

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>

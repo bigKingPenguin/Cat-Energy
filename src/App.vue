2ee676e7732d7cebb51da367ce1e975effd4688d
Emiliia <emiliatoropova@yandex.ru>
2023-02-13 18:13:57 +0200

    Создает блок Promo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <Navbar></Navbar>
+    <Promo></Promo>
   </div>
 </template>
 
 <script setup>
 
 import Navbar from "@/components/Navbar/Navbar.vue";
+import Promo from "@/components/Promo/Promo.vue";
 </script>
 
 <style scoped>

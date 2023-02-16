6de19c584400225d8084ae380bdf43e2a7cf876e
Emiliia <emiliatoropova@yandex.ru>
2023-02-16 21:57:57 +0200

    Стилизует Header

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,24 +1,117 @@
 <template>
-  <header>
-    <navigation-logo class="logo"></navigation-logo>
-    <!--    TODO: переименовать в "открыть/закрыть меню" при автоматизации-->
-    <button class="button button--navigation">
-      Переключить меню
+  <header class="header">
+    <NavigationLogo class="logo"></NavigationLogo>
+    <LogoName class="logo__name"></LogoName>
+    <button
+      @click="navbarToggle"
+      class="button button--navigation"
+    >
+      <span class="visually-hidden">
+        {{ navbarIsOpen ? 'Закрыть меню' : 'Открыть меню' }}
+      </span>
+      <NavbarBurger
+        :class="{
+         'burger--cross': !navbarIsOpen
+       }"
+      >
+      </NavbarBurger>
     </button>
-    <navigation-list class="navigation"></navigation-list>
+    <NavigationList
+      class="navigation"
+      :class="{
+           'navigation--hidden': navbarIsOpen
+          }"
+    >
+    </NavigationList>
   </header>
 </template>
 
 <script>
-import NavigationLogo from '@/components/Navbar/components/NavigationLogo.vue';
-import NavigationList from '@/components/Navbar/components/NavigationList.vue';
+  import {ref} from 'vue';
+  import NavigationLogo from '@/components/Navbar/components/NavigationLogo.vue';
+  import NavigationList from '@/components/Navbar/components/NavigationList.vue';
+  import NavbarBurger from '@/components/Navbar/components/NavbarBurger.vue';
+  import LogoName from '@/components/icons/LogoName.vue';
 
-export default {
-  name: 'Navbar',
-  components: {NavigationList, NavigationLogo},
-};
+  export default {
+    name: 'Navbar',
+    components: {NavbarBurger, NavigationList, NavigationLogo, LogoName},
+    setup() {
+      const navbarIsOpen = ref(false);
+      const navbarToggle = () => {
+        navbarIsOpen.value = !navbarIsOpen.value;
+      };
+      return {
+        navbarIsOpen, navbarToggle,
+      };
+    },
+
+  };
 </script>
 
 <style lang="scss" scoped>
+  .header {
+    display: flex;
+    width: 100%;
+    padding-top: 12px;
+    padding-bottom: 12px;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    background-color: var(--color-white);
 
+    @media (min-width: 768px) {
+      position: absolute;
+      width: calc(100% - var(--padding-tablet) * 2);
+      left: var(--padding-tablet);
+      padding-top: 0;
+      padding-bottom: 0;
+      top: 24px;
+      background-color: transparent;
+    }
+
+    @media (min-width: 1440px) {
+      width: calc(100% - var(--padding-desktop) * 2);
+      left: var(--padding-desktop);
+      top: 58px;
+    }
+  }
+
+  .navigation {
+    position: absolute;
+    top: var(--header-mobile-height);
+    left: calc(-1 * var(--padding-mobile));
+    display: flex;
+    width: calc(100% + 2 * var(--padding-mobile));
+    transition: transform 0.6s ease;
+    z-index: -1;
+
+    &--hidden {
+      transform: translateY(-1000px);
+    }
+
+    @media (min-width: 768px) {
+      z-index: 1;
+      position: static;
+    }
+
+  }
+
+  .button--navigation {
+    @media (min-width: 768px) {
+      display: none;
+    }
+  }
+
+  .logo {
+    &__name {
+      @media (min-width: 768px) {
+        display: none;
+      }
+    }
+
+    @media (min-width: 768px) {
+      margin-right: 44px;
+    }
+  }
 </style>
--- src/components/Navbar/components/NavigationLogo.vue
+++ src/components/Navbar/components/NavigationLogo.vue
@@ -3,25 +3,60 @@
     class="logo"
     href="#"
   >
-    <logo-mobile class="logo__mobile"></logo-mobile>
-    <logo-name class="logo__name"></logo-name>
-    <logo-tablet class="logo__tablet"></logo-tablet>
-    <logo-desktop class="logo__desktop"></logo-desktop>
+    <LogoMobile class="logo__mobile"></LogoMobile>
+    <LogoTablet class="logo__tablet"></LogoTablet>
+    <LogoDesktop class="logo__desktop"></LogoDesktop>
   </a>
 </template>
 
 <script>
-import LogoMobile from '@/components/icons/LogoMobile.vue';
-import LogoName from '@/components/icons/LogoName.vue';
-import LogoTablet from '@/components/icons/LogoTablet.vue';
-import LogoDesktop from '@/components/icons/LogoDesktop.vue';
+  import LogoMobile from '@/components/icons/LogoMobile.vue';
+  import LogoName from '@/components/icons/LogoName.vue';
+  import LogoTablet from '@/components/icons/LogoTablet.vue';
+  import LogoDesktop from '@/components/icons/LogoDesktop.vue';
 
-export default {
-  name: 'NavigationLogo',
-  components: {LogoDesktop, LogoTablet, LogoName, LogoMobile},
-};
+  export default {
+    name: 'NavigationLogo',
+    components: {LogoDesktop, LogoTablet, LogoName, LogoMobile},
+  };
 </script>
 
 <style lang="scss" scoped>
 
+  .logo {
+    justify-content: space-between;
+
+    &:hover {
+      opacity: 0.8;
+    }
+
+    &:active {
+      opacity: 0.6;
+    }
+
+    &__mobile {
+      @media (min-width: 768px) {
+        display: none;
+      }
+    }
+
+    &__tablet {
+      display: none;
+
+      @media (min-width: 768px) {
+        display: block;
+      }
+      @media (min-width: 1440px) {
+        display: none;
+      }
+    }
+
+    &__desktop {
+      display: none;
+
+      @media (min-width: 1440px) {
+        display: block;
+      }
+    }
+  }
 </style>

23d858822305f6bf793fcf200a87959ace42202a
Emiliia <emiliatoropova@yandex.ru>
2023-02-20 18:23:40 +0200

    Стилизует Header

--- src/components/Promo/Promo.vue
+++ src/components/Promo/Promo.vue
@@ -1,48 +1,73 @@
 <template>
   <section class="promo">
-    <!--    <h1 class="promo__header">-->
-    <!--      Функциональное питание для котов-->
-    <!--    </h1>-->
-    <!--    <p class="promo__description">-->
-    <!--      Занялся собой? Займись котом!-->
-    <!--    </p>-->
-    <!--    <PromoImage-->
-    <!--      v-for="img in promoImagesArr"-->
-    <!--      :key="img.src"-->
-    <!--      :imageProp="img"-->
-    <!--    >-->
-    <!--    </PromoImage>-->
-    <!--    <a-->
-    <!--      href="#"-->
-    <!--      class="button button&#45;&#45;green"-->
-    <!--    >-->
-    <!--      Подобрать программу-->
-    <!--    </a>-->
-    <div class="promo__img-wrapper">
-      <img src="src/assets/img/promo/background-cat-desktop@1x.png" class="promo__background" width="882" height="694">
+    <div class="promo__content-wrapper">
+      <h1 class="promo__title">
+        Функциональное питание для котов
+      </h1>
+      <p class="promo__tagline">
+        Занялся собой? Займись котом!
+      </p>
+      <PromoImage
+        v-for="img in promoImagesArr"
+        :key="img.src"
+        :imageProp="img"
+      >
+      </PromoImage>
     </div>
+    <div class="promo__img-wrapper">
+      <img
+        src="src/assets/img/promo/background-cat-mobile@1x.png"
+        width="320"
+        height="302"
+        class="promo__img promo__img--mobile"
+      >
+      <img
+        src="src/assets/img/promo/background-cat-desktop@1x.png"
+        width="720"
+        height="694"
+        class="promo__img promo__img--desktop"
+      >
+    </div>
+    <Button
+      href="#"
+      className="button--green button--promo"
+      content="Подобрать программу"
+    >
+    </Button>
   </section>
 </template>
 
 <script>
   import PromoImage from '@/components/Promo/components/PromoImage.vue';
   import SpecialItems from '@/components/Special/components/SpecialItems.vue';
+  import Button from '@/components/common/Button.vue';
 
   export default {
     name: 'Promo',
-    components: {SpecialItems, PromoImage},
+    components: {Button, SpecialItems, PromoImage},
     setup() {
       const promoImagesArr = [
+        {
+          src: 'src/assets/img/promo/index-can-mobile@1x.png',
+          width: '280',
+          height: '270',
+          alt: 'Упаковка корма Cat Energy',
+          class: 'promo__pack promo__pack--mobile',
+        },
         {
           src: 'src/assets/img/promo/index-can-tablet@1x.png',
           width: '709',
           height: '609',
           alt: 'Упаковка корма Cat Energy',
-          class: 'promo__img promo__img--tablet',
+          class: 'promo__pack promo__pack--tablet',
         },
-        //{
-        //TODO: убрать отсюда пропс или нет?
-        //}
+        {
+          src: 'src/assets/img/promo/index-can-desktop@1x.png',
+          width: '552',
+          height: '499',
+          alt: 'Упаковка корма Cat Energy',
+          class: 'promo__pack promo__pack--desktop',
+        },
       ];
       return {
         promoImagesArr,
@@ -53,27 +78,188 @@
 
 <style lang="scss" scoped>
   .promo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
 
-    //TODO: мб сделать один враппер для картинок под мобилку и десктоп
-    &__img-wrapper {
-      display: none;
-      position: relative;
-      width: 70%;
-      margin-left: auto;
-      margin-right: calc(-1 * var(--padding-desktop));
+    @media (min-width: 768px) {
+      align-items: flex-start;
     }
 
-    @media (min-width: 1440px) {
-      &__img-wrapper {
-        display: block;
+    &__content-wrapper {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      max-width: 280px;
+      padding-top: 26px;
+      padding-bottom: 3px;
+
+      @media (min-width: 768px) {
+        max-width: unset;
+        padding-top: 50px;
+        padding-left: 87px;
+        position: relative;
       }
 
-      &__background {
+      @media (min-width: 768px) {
+        padding-top: 99px;
+        padding-left: 80px;
+      }
+    }
+
+    &__title {
+      max-width: 450px;
+      margin-bottom: 23px;
+      font-size: 36px;
+      font-weight: 400;
+      line-height: 37px;
+      color: var(--color-white);
+      text-align: center;
+
+      @media (min-width: 768px) {
+        margin-bottom: 37px;
+        font-size: 60px;
+        line-height: 61px;
+        color: var(--color-black);
+        text-align: left;
+      }
+
+      @media (min-width: 1440px) {
+        margin-bottom: 37px;
+      }
+    }
+
+    &__tagline {
+      margin-bottom: 28px;
+      font-size: 14px;
+      line-height: 16px;
+      color: var(--color-white);
+      text-transform: uppercase;
+      text-align: center;
+
+      @media (min-width: 768px) {
+        margin-bottom: 44px;
+        font-size: 20px;
+        line-height: 24px;
+        color: var(--color-black);
+        text-align: left;
+      }
+
+      @media (min-width: 1440px) {
+        margin-bottom: 47px;
+      }
+    }
+
+    &__pack {
+
+      &--mobile {
+        @media (min-width: 768px) {
+          display: none;
+        }
+      }
+
+      &--tablet {
+        display: none;
+
+        @media (min-width: 768px) {
+          display: block;
+          position: absolute;
+          top: 253px;
+          left: 0;
+          z-index: -1;
+        }
+
+        @media (min-width: 1440px) {
+          display: none;
+        }
+      }
+
+      &--desktop {
+        display: none;
+
+        @media (min-width: 1440px) {
+          display: block;
+          position: absolute;
+          top: 18px;
+          left: 448px;
+        }
+      }
+    }
+
+    &__img-wrapper {
+      display: flex;
+      position: absolute;
+      width: 100%;
+      min-width: 320px;
+      height: 302px;
+      margin-right: calc(-1 * var(--padding-mobile));
+      margin-left: calc(-1 * var(--padding-mobile));
+      z-index: -1;
+
+      @media (min-width: 768px) {
+        width: auto;
+      }
+
+      @media (min-width: 1440px) {
+        width: 51.5%;
+        min-width: unset;
+        height: 696px;
         position: absolute;
         top: 0;
         right: 0;
+      }
+    }
+
+
+    &__img {
+      width: 100%;
+      object-fit: cover;
+
+      &--mobile {
+        display: block;
+        @media (min-width: 768px) {
+          display: none;
+        }
+
+        @media (min-width: 1440px) {
+          display: none;
+        }
+      }
+
+      &--tablet {
+        display: none;
+
+        @media (min-width: 768px) {
+          display: block;
+        }
+
+        @media (min-width: 1440px) {
+          display: none;
+        }
+      }
+
+      &--desktop {
+        display: none;
+
+        @media (min-width: 1440px) {
+          display: block;
+        }
+      }
+    }
+
+    .button {
+      &--promo {
         width: 100%;
-        object-fit: cover;
+
+        @media (min-width: 768px) {
+          width: max-content;
+          margin-left: 82px;
+        }
+
+        @media (min-width: 1440px) {
+          margin-left: 80px;
+        }
       }
     }
   }
---
+++ src/components/common/Button.vue
@@ -0,0 +1,97 @@
+<template>
+  <component
+    class="button"
+    :is="props.href ? 'a' : 'button'"
+    :class="props.className"
+    :href="props.href"
+    :type="!props.href ? props.action : null"
+  >
+    <span>
+      {{ props.content }}
+    </span>
+    <span
+      v-if="props.iconName"
+      class="button__icon"
+    >
+      <component :is="props.iconName"></component>
+    </span>
+  </component>
+
+</template>
+
+<script>
+  export default {
+    name: 'Button',
+    props: {
+      href: {
+        type: String,
+        default: '',
+      },
+      className: {
+        type: String,
+        default: '',
+      },
+      action: {
+        type: String,
+        default: 'button',
+      },
+      content: {
+        type: String,
+        default: '',
+      },
+      iconName: {
+        type: String,
+        default: '',
+      },
+    },
+
+    setup(props) {
+      return {
+        props,
+      };
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .button {
+    display: flex;
+    width: max-content;
+    min-height: 40px;
+    justify-content: center;
+    align-items: center;
+    font-size: 14px;
+    line-height: 18px;
+    text-transform: uppercase;
+    padding: 10px 26px;
+
+    @media (max-width: 768px) {
+      font-size: 20px;
+      line-height: 18px;
+      padding: 18px 27px;
+    }
+
+    @media (max-width: 1440px) {
+      font-size: 20px;
+      line-height: 18px;
+      padding: 18px 27px;
+    }
+
+    &--green {
+      background-color: var(--color-primary);
+      color: var(--color-white);
+
+      &:hover {
+        background-color: var(--color-primary-dark);
+      }
+
+      &:active {
+        background-color: var(--color-primary-dark);
+
+        & > span {
+          opacity: 0.3;
+        }
+      }
+    }
+  }
+</style>
